6020bb00efb14833ab1a7c7713b96bc285e8c052
samkc-0 <sam.k.c.0@icloud.com>
2025-02-13 16:53:24 +0100

    add github actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/samkc-0.github.io/", // Required for GitHub Pages
+  base: "/samkc-0.github.io", // Required for GitHub Pages
   build: {
     outDir: "dist", // This is the folder you will deploy
   },
